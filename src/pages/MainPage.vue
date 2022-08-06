7679843052598dfd6b61ed2b9b6fc3f7c68b005b
sy.choi <sy.choi@cndfactory.com>
2022-08-07 03:12:54 +0900

    퍼블리싱 환경 세팅

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="mainpage">
+    <div class="second-title">이 상품 어때요?</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage"
+}
+</script>
+
+<style>
+.second-title {
+  font-size: 1.6em;
+  font-weight: bold;
+  line-height: 4em;
+  height: 4em;
+}
+</style>
